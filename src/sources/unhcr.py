88384648559f46ad1469fca67ac495bda72dfe8e
belisards <adrianobf@gmail.com>
2025-12-12 14:08:42 -0300

    fix: add user-agent header to unhcr api calls

--- src/sources/unhcr.py
+++ src/sources/unhcr.py
@@ -6,6 +6,10 @@ from utils import UNHCR_DATA_PATH
 METADATA_LIST_URL = "https://microdata.unhcr.org/index.php/api/catalog/search?ps=9999999&sort_by=created&sort_order=desc"
 DATASET_EXPORT_URL = "https://microdata.unhcr.org/index.php/metadata/export/{}/json"
 
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+}
+
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 def fetch_metadata_list():
@@ -16,7 +20,7 @@ def fetch_metadata_list():
     - pd.DataFrame: The metadata as a pandas DataFrame.
     """
     try:
-        response = requests.get(METADATA_LIST_URL)
+        response = requests.get(METADATA_LIST_URL, headers=HEADERS)
         response.raise_for_status()
         data = response.json()
         data = data["result"]["rows"]
@@ -38,7 +42,7 @@ def fetch_dataset(id):
     Returns:
     - dict: Dataset information
     """
-    response = requests.get(DATASET_EXPORT_URL.format(id))
+    response = requests.get(DATASET_EXPORT_URL.format(id), headers=HEADERS)
     response.raise_for_status()
     data = response.json()
     data["id"] = id
